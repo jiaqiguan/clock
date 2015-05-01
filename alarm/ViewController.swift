d0225dbf784e978147eeabb90330d0c8fd22871d
Guan Jiaqi <guanjiaqi@GuanmatoMacBook-Air.local>
2015-05-01 14:15:42 -0700

    May 1

--- alarm/ViewController.swift
+++ alarm/ViewController.swift
@@ -10,9 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minutehand: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let date = NSDate()
+        
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
+        
+        let hour = components.hour
+        
+        let minutes = components.minute
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+
+        
+        time.text = " \(minutes) "
+        var c = (CGFloat(minutes-15))/30.0*3.1415926
+        var f = CGFloat(c)
+        self.minutehand.layer.anchorPoint=CGPoint(x:0.0,y:0.5)
+        var t = CGAffineTransformMakeRotation(f)
+      
+        
+        self.minutehand.transform = t
+        
+        
+    }
+    @IBOutlet weak var time: UILabel!
+    
+    func update() {
+        let date = NSDate()
+        
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
+        
+        let hour = components.hour
+        
+        let minutes = components.minute
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        
+        
+        time.text = " \(minutes) "
+        var c = (CGFloat(minutes-15))/30.0*3.1415926
+        var f = CGFloat(c)
+        self.minutehand.layer.anchorPoint=CGPoint(x:0.0,y:0.5)
+        var t = CGAffineTransformMakeRotation(f)
+        
+        
+        self.minutehand.transform = t
+        
     }
 
     override func didReceiveMemoryWarning() {
